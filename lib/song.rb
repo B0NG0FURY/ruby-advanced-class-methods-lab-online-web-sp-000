b0477ef6298948a0f5ff75f42d49221cd37d4f0c
John Troutman <jtroutmanman@gmail.com>
2020-09-29 15:48:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,4 +32,11 @@ class Song
   def self.find_by_name(name)
     self.all.find {|song| song.name == name}
   end
+  
+  def self.find_or_create_by_name(name)
+    if !self.all.find {|song| song.name == name}
+    else
+      song = self.new
+      song.name = name
+  end
 end
